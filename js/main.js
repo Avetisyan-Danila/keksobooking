10e1d65de77fc6b7c94ae2474d5e827139fa4d74
Danila <avetisyandanila21@yandex.ru>
2021-06-05 16:55:24 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function getRandomNum(min, max, digits) {
+  if (min > max || min === max || min < 0 || max <= 0 || digits < 0 || typeof(max) !== 'number' || typeof(min) !== 'number' || typeof(digits) !== 'number') {
+    return ('Задан неверный диапазон! Укажите другие числа.');
+  }
+
+  return +(Math.random() * (max - min) + min).toFixed(digits);
+}
+
+console.log(getRandomNum(1, 15, 3));
