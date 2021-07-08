64d36b5c96a022eb00ef3f131ee703e6bf0f6d25
Danila <avetisyandanila21@yandex.ru>
2021-07-08 13:27:54 +0300

    Правда или действие (часть 2)

--- js/form-validation.js
+++ js/form-validation.js
@@ -30,3 +30,44 @@ formRoomQuantity.addEventListener('change', () => {
     formGuestQuantityOptions[3].removeAttribute('disabled');
   }
 });
+
+const formHouseType = adForm.querySelector('#type');
+const formHousePrice = adForm.querySelector('#price');
+
+formHouseType.addEventListener('change', () => {
+  if (formHouseType.value === 'bungalow') {
+    formHousePrice.setAttribute('placeholder', '0');
+    formHousePrice.setAttribute('min', '0');
+  }
+
+  else if (formHouseType.value === 'flat') {
+    formHousePrice.setAttribute('placeholder', '1000');
+    formHousePrice.setAttribute('min', '1000');
+  }
+
+  else if (formHouseType.value === 'hotel') {
+    formHousePrice.setAttribute('placeholder', '3000');
+    formHousePrice.setAttribute('min', '3000');
+  }
+
+  else if (formHouseType.value === 'house') {
+    formHousePrice.setAttribute('placeholder', '5000');
+    formHousePrice.setAttribute('min', '5000');
+  }
+
+  else if (formHouseType.value === 'palace') {
+    formHousePrice.setAttribute('placeholder', '10000');
+    formHousePrice.setAttribute('min', '10000');
+  }
+});
+
+const formTimeIn = adForm.querySelector('#timein');
+const formTimeOut = adForm.querySelector('#timeout');
+
+formTimeIn.addEventListener('change', () => {
+  formTimeOut.value = formTimeIn.value;
+});
+
+formTimeOut.addEventListener('change', () => {
+  formTimeIn.value = formTimeOut.value;
+});
